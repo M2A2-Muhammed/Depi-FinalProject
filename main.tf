0ade66074c18eafab079208fd34947e1a6bf0ca2
mu <muhammed.abdelghafar@outlook.com>
2024-10-01 11:18:14 +0000

    enable public ip

--- main.tf
+++ main.tf
@@ -92,6 +92,7 @@ resource "aws_instance" "web-server" {
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
 
+  associate_public_ip_address = true
 
   tags = {
     Name = "web-server"
