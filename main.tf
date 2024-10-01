53739c9db00cf83a3b830ed42c1a6de5bf9dbdc9
mu <muhammed.abdelghafar@outlook.com>
2024-10-01 12:38:55 +0000

    adding ssh cache

--- main.tf
+++ main.tf
@@ -70,8 +70,15 @@ resource "aws_security_group" "allow_ssh_http" {
   }
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow HTTP from anywhere
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"] # Allow HTTP from anywhere
   }
@@ -81,6 +88,10 @@ resource "aws_security_group" "allow_ssh_http" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"] # Allow all outbound traffic
+  }
+
+  tags = {
+    Name = "allow ssh & http"
   }
 }
 
