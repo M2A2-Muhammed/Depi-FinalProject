88796690f655b4d47209e631f470249945c1073b
mu <muhammed.abdelghafar@outlook.com>
2024-10-01 13:07:23 +0000

    Destroy again

--- main.tf
+++ main.tf
@@ -19,103 +19,103 @@ provider "aws" {
   secret_key = var.aws_secret_access_key
 }
 
-resource "aws_key_pair" "github_actions" {
-  key_name   = "github-actions-key"
-  public_key = file(var.ssh_public_key)
-}
+# resource "aws_key_pair" "github_actions" {
+#   key_name   = "github-actions-key"
+#   public_key = file(var.ssh_public_key)
+# }
 
-# Create a VPC
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-}
+# # Create a VPC
+# resource "aws_vpc" "main" {
+#   cidr_block = "10.0.0.0/16"
+# }
 
-# Create a public subnet
-resource "aws_subnet" "public" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = var.availability_zone
-}
+# # Create a public subnet
+# resource "aws_subnet" "public" {
+#   vpc_id                  = aws_vpc.main.id
+#   cidr_block              = "10.0.1.0/24"
+#   map_public_ip_on_launch = true
+#   availability_zone       = var.availability_zone
+# }
 
-# Create an internet gateway
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-}
+# # Create an internet gateway
+# resource "aws_internet_gateway" "igw" {
+#   vpc_id = aws_vpc.main.id
+# }
 
-# Create a route table
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
+# # Create a route table
+# resource "aws_route_table" "public" {
+#   vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-}
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.igw.id
+#   }
+# }
 
-# Associate the route table with the public subnet
-resource "aws_route_table_association" "public_association" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public.id
-}
+# # Associate the route table with the public subnet
+# resource "aws_route_table_association" "public_association" {
+#   subnet_id      = aws_subnet.public.id
+#   route_table_id = aws_route_table.public.id
+# }
 
-# Create a security group to allow SSH and HTTP traffic
-resource "aws_security_group" "allow_access" {
-  vpc_id = aws_vpc.main.id
+# # Create a security group to allow SSH and HTTP traffic
+# resource "aws_security_group" "allow_access" {
+#   vpc_id = aws_vpc.main.id
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Allow SSH from anywhere
-  }
+#   ingress {
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"] # Allow SSH from anywhere
+#   }
 
-  ingress {
-    from_port   = 9090
-    to_port     = 9090
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Allow HTTP from anywhere
-  }
+#   ingress {
+#     from_port   = 9090
+#     to_port     = 9090
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"] # Allow HTTP from anywhere
+#   }
 
-  ingress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Allow HTTP from anywhere
-  }
+#   ingress {
+#     from_port   = 3000
+#     to_port     = 3000
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"] # Allow HTTP from anywhere
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"] # Allow all outbound traffic
-  }
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"] # Allow all outbound traffic
+#   }
 
-  tags = {
-    Name = "Allow SSH, Prometheus & Grafana"
-  }
-}
+#   tags = {
+#     Name = "Allow SSH, Prometheus & Grafana"
+#   }
+# }
 
 
-resource "aws_instance" "web-server" {
-  ami                    = var.ami
-  instance_type          = var.instance_type
-  key_name               = aws_key_pair.github_actions.key_name
-  subnet_id              = aws_subnet.public.id
-  vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
+# resource "aws_instance" "web-server" {
+#   ami                    = var.ami
+#   instance_type          = var.instance_type
+#   key_name               = aws_key_pair.github_actions.key_name
+#   subnet_id              = aws_subnet.public.id
+#   vpc_security_group_ids = [aws_security_group.allow_access.id]
 
-  associate_public_ip_address = true
+#   associate_public_ip_address = true
 
-  tags = {
-    Name = "web-server"
-  }
+#   tags = {
+#     Name = "web-server"
+#   }
 
-  # User data to install OpenSSH server
-  user_data = <<-EOF
-              #!/bin/bash
-              apt update -y
-              apt install -y openssh-server
-              systemctl enable sshd
-              systemctl start sshd
-              EOF
+#   # User data to install OpenSSH server
+#   user_data = <<-EOF
+#               #!/bin/bash
+#               apt update -y
+#               apt install -y openssh-server
+#               systemctl enable sshd
+#               systemctl start sshd
+#               EOF
 
-}
+# }
