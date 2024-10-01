9ccc1cdee18169bc7d8096aa6c0d5dc2942eae02
mu <muhammed.abdelghafar@outlook.com>
2024-10-01 08:33:16 +0000

    change ssh user

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,7 @@
 output "instance_ip" {
   value = aws_instance.web-server.public_ip
 }
+
+output "default_user" {
+  value = data.aws_ami.selected.virtualization_type == "hvm" ? "ec2-user" : "ubuntu" # Adjust based on AMI
+}
